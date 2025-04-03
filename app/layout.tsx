b4340ad79df95aaec4b8787ceb2e2367a5de4993
afoim <sudo@onani.cn>
2025-04-03 09:48:22 +0800

    在布局中添加 Umami 分析脚本以增强网站分析功能

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar'
 import './globals.css'
 import 'highlight.js/styles/github-dark.css'  // 深色主题
 import 'highlight.js/styles/github.css'       // 浅色主题
+import Script from 'next/script'
 
 export const metadata = {
   title: '二叉树树的博客',
@@ -15,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
+      <head>
+        <Script
+          src="https://analytics.umami.is/script.js"
+          data-website-id="a66a5fd4-98b0-4108-8606-cb7094f380ac"    // 替换为你的网站 ID
+          async
+          defer
+        />
+      </head>
       <body>
         <Navbar />
         {children}
